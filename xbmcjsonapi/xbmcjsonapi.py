624df4a9421a021b110a5bc37e743adc34c6af3d
Manuel Sabban <manu@sabban.eu>
2014-05-23 15:56:05 +0200

    Transformation of transport to a new class TODO:  * Write at least a new transport for TCP in order to have access all features  * Move the transport classes to a new file.

--- xbmcjsonapi/xbmcjsonapi.py
+++ xbmcjsonapi/xbmcjsonapi.py
@@ -6,48 +6,56 @@ import string
 import json
 import urllib2
 import ConfigParser
+import os
 
-
-json_introspect='{ "jsonrpc": "2.0", "method": "JSONRPC.Introspect", "id": 1 }'
 cache={}
 
-config = ConfigParser.RawConfigParser()
 
-config.read(os.path.expanduser('~') + '/.xbmcjsonapi')
-username=config.get('http','username')
-password=config.get('http','password')
-json_url=config.get('http','json_url')
-host=config.get('http','host')
-
-class xmbcjsonapitransport:
+class xbmcjsonapitransport:
     def __init__(self):
         pass
+    def request(self,jsonrequest):
+        pass
+        
 
-def http_json_request(json_request):
-    # base64 encode the username and password
-    auth = base64.encodestring('%s:%s' % (username, password)).replace('\n', '')
-    url = 'http://%s%s%s' % (host, json_url, json_request)
+class HTTPtransport(xbmcjsonapitransport):
+    json_introspect='{ "jsonrpc": "2.0", "method": "JSONRPC.Introspect", "id": 1 }'
+    config = ConfigParser.RawConfigParser()
+    config.read(os.path.expanduser('~') + '/.xbmcjsonapi')
+    username=config.get('http','username')
+    password=config.get('http','password')
+    port=config.get('http','port')
+    json_url=config.get('http','json_url')
+    host=config.get('http','host')
 
-    # write the Authorization header like: 'Basic base64encode(username + ':' + password)
-    headers = {'Authorization' : 'Basic %s' % auth, 
-               'Content-Type' : 'application/json',
-               'Content-Length' : '%s' % len(json_request)}
+    def __init__(self):
+      pass  
+        
+    def request(self, json_request):
+        # base64 encode the username and password
+        auth = base64.encodestring('%s:%s' % (self.username, self.password)).replace('\n', '')
+        url = 'http://%s:%s%s%s' % (self.host, self.port, self.json_url, json_request)
+        # write the Authorization header like: 'Basic base64encode(username + ':' + password)
+        headers = {'Authorization' : 'Basic %s' % auth, 
+                   'Content-Type' : 'application/json',
+                   'Content-Length' : '%s' % len(json_request)}
     
         # get the response
-    req = urllib2.Request( 'http://%s%s' % (host,json_url), json_request, headers)
-    response = urllib2.urlopen(req)
-    json = response.read()
-    return json
+        req = urllib2.Request( url, json_request, headers)
+        response = urllib2.urlopen(req)
+        json = response.read()
+        return json
 
 
 class xbmcjsonapi:
     def __init__(self):
         pass
 
+transport_instance=HTTPtransport()
 
 def namespaces():
     namespaces=[]
-    for method in json.loads(http_json_request(json_introspect))['result']['methods']:
+    for method in json.loads(transport_instance.request(transport_instance.json_introspect))['result']['methods']:
         if method.split('.')[0] not in namespaces:
             namespaces.append(method.split('.')[0])
     return namespaces
@@ -60,7 +68,7 @@ for namespace in namespaces():
     """%(namespace,namespace)
     exec (s)
 
-cache=json.loads(http_json_request(json_introspect))['result']['methods']
+cache=json.loads(transport_instance.request(transport_instance.json_introspect))['result']['methods']
 for namespace in namespaces():
     for key in cache:
         if namespace in key:
@@ -76,7 +84,7 @@ for namespace in namespaces():
                 json_request=json_request+'}'
             else:
                 json_request=json_request+json.dumps(kwargs)+'}'
-            return http_json_request(json_request)
+            return transport_instance.request(json_request)
             """ % (function_name, cache[key]['description'], cache[key]['params'], cache[key]['returns'], namespace, function_name)
             exec(s)
             s="""setattr(%s,'%s',%s)""" % (namespace, function_name, function_name)
